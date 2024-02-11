1dc94903cfd1728471798a667e98fa9e5540d5c2
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-02-11 20:35:41 +0530

    Added pages routes to the frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 function App() {
 
 
   return (
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Routes>
+      <Route path="/" element = {<Home />}/>
+      <Route path="/about" element = {<About />}/>
+      <Route path="/sign-in" element = {<Signin />}/>
+      <Route path="/sign-up" element = {<SignUp />}/>
+      <Route path="/dashboard" element = {<Dashboard />}/>
+      <Route path="/projects" element = {<Projects />}/>
+    </Routes>
   )
 }
 
