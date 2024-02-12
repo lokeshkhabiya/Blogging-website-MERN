a17938d7624b76f1e7d1052d72095bd06c2e643a
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-02-13 00:35:11 +0530

    Minor changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,19 +5,23 @@ import Signin from "./pages/Signin";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
 
 
   return (
-    <Routes>
-      <Route path="/" element = {<Home />}/>
-      <Route path="/about" element = {<About />}/>
-      <Route path="/sign-in" element = {<Signin />}/>
-      <Route path="/sign-up" element = {<SignUp />}/>
-      <Route path="/dashboard" element = {<Dashboard />}/>
-      <Route path="/projects" element = {<Projects />}/>
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element = {<Home />}/>
+        <Route path="/about" element = {<About />}/>
+        <Route path="/sign-in" element = {<Signin />}/>
+        <Route path="/sign-up" element = {<SignUp />}/>
+        <Route path="/dashboard" element = {<Dashboard />}/>
+        <Route path="/projects" element = {<Projects />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,8 +5,6 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-     <BrowserRouter>
       <App />
-    </BrowserRouter>,
 )
 
