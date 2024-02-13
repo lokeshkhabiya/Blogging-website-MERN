5281024705f457b28ec8cade37f96f682d934826
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-02-13 09:01:34 +0530

    updated header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,8 +1,27 @@
+import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Navbar className = "border-b-2 "> 
+        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white '>
+            <span className='px-2 py-1 bg-gradient-to-r from-cyan-400 to-pink-600 rounded-lg text-white'>Lokesh's</span>
+            Blog
+        </Link>
+
+        <form>
+          <TextInput 
+            type='text'
+            placeholder='Search...'
+            rightIcon={AiOutlineSearch}
+          />
+        </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+          <AiOutlineSearch /> 
+        </Button>
+    </Navbar>
   )
 }
 
-export default Header
+export default Header;
